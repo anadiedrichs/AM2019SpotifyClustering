169c9efac78d5873c2d5ee75e2f294ba145842d5
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2019-05-27 12:30:21 -0300

    adding scripts from old repo

---
+++ logs-test.R
@@ -0,0 +1,15 @@
+## 
+
+start_time <- Sys.time()
+source("0-get-data-2.R")
+end_time <- Sys.time()
+# end_time - start_time
+# Time difference of 4.244175 mins
+
+# limpiar working space
+
+start_time <- Sys.time()
+source("0-get-data.R")
+end_time <- Sys.time()
+# end_time - start_time
+# Time difference of 4.207372 mins
